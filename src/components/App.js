83924da727854be1b8e8dea04c3fba096d8e4ad1
ZohaibA99 <118318498+ZohaibA99@users.noreply.github.com>
2023-07-27 18:37:52 -0400

    lesson 5

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,33 @@ import { GlobalStyle } from "./GlobalStyle";
 import { Layout } from "./Layout/Layout";
 import {RecipeForm} from "./RecipeForm/RecipeForm";
 
+
+//Component Life Cycle
+//constructor, getDerivedStateFromProps, render, shouldCompUpdate, render, componentDidUpdate, componenentWillUnmount
+
+//render > componentDidMount > getItem > setState > update > render > compDidUpdate > setItem
 export class App extends Component{
     state = {
-        recipes: initialRecipes,
+        recipes: [],
+    }
+
+    componentDidMount() {
+        const savedRecipes = localStorage.getItem("recipes");
+        console.log("get item >")
+        if(savedRecipes != null){
+            this.setState({recipes: JSON.parse(savedRecipes)});
+        }
+        else{
+            this.setState({recipes: initialRecipes});
+        }
+        console.log("Set state > Did mount");
+    }
+
+    componentDidUpate(prevState, prevProps){
+        if(prevState.recipes !== this.state.recipes){
+            localStorage.setItem('recipes', JSON.stringify(this.state.recipes));
+        }
+        console.log("set Item > updated")
     }
 
     addRecipe = newRecipe => {
@@ -23,6 +47,7 @@ export class App extends Component{
     };
 
     render() {
+        console.log("render method");
         return (
             <Layout>
                 <RecipeForm onSave={this.addRecipe}></RecipeForm>
@@ -33,13 +58,24 @@ export class App extends Component{
     }
 }
 
-// export const App = () => {
-//     return (
-//         <Layout>
-//             <div>Recipe Form</div>
-//             <RecipeList items={recipes} />
-//             <div>Image modal</div>
-//             <GlobalStyle />
-//         </Layout>
-//     );
-// };
+
+///Lesson 5 handling render errors
+// export class App extends Component{
+//     state = {hasError: false};
+
+//     componentDidCatch(error, info){
+//         //If this method was called, then there is an error.
+//         //set the state
+//         this.setState({hasError: true});
+//         //you can also send an error report to yoiur analytics service
+//         //logErrorToMYService(error, info);
+//     }
+
+//     render() {
+//         if(this.state.hasError) {
+//             return <h1>Something went wrong, please try again later;</h1>
+//         }
+
+//         return this.props.children;
+//     }
+// }
