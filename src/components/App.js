5629641ffcdc872b6dd8fbb625e1b794bf9613a1
ZohaibA99 <118318498+ZohaibA99@users.noreply.github.com>
2023-07-30 12:26:48 -0400

    lesson 6 http requests using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,81 +1,111 @@
-import { Component } from 'react';
-import { RecipeList } from './RecipeList/RecipeList';
-import initialRecipes from "../recipes.json";
-import { GlobalStyle } from "./GlobalStyle";
-import { Layout } from "./Layout/Layout";
-import {RecipeForm} from "./RecipeForm/RecipeForm";
+import { Component } from "react";
+import { Layout } from "./Layout";
+import { BreedSelect } from "./BreedSelect";
+import { fetchDogByBreed } from "api";
+import { Dog } from "./Dog";
+import { ErrorMessage } from "./ErrorMessage";
+import { DogSkeleton } from "./DogSkeleton";
 
+const ERROR_MSG = 
+    `Something went wrong please try again later`;
 
-//Component Life Cycle
-//constructor, getDerivedStateFromProps, render, shouldCompUpdate, render, componentDidUpdate, componenentWillUnmount
-
-//render > componentDidMount > getItem > setState > update > render > compDidUpdate > setItem
 export class App extends Component{
     state = {
-        recipes: [],
+        dog: null,
+        isLoading: false,
+        error: null,
     }
 
-    componentDidMount() {
-        const savedRecipes = localStorage.getItem("recipes");
-        console.log("get item >")
-        if(savedRecipes != null){
-            this.setState({recipes: JSON.parse(savedRecipes)});
+    fetchDog = async breedId => {
+        try {
+          this.setState({ isLoading: true, error: null });
+          const fetchedDog = await fetchDogByBreed(breedId);
+          this.setState({ dog: fetchedDog });
+          console.log(this.state.dog)
+        } catch (error) {
+          this.setState({ error: ERROR_MSG });
+        } finally {
+          this.setState({ isLoading: false });
         }
-        else{
-            this.setState({recipes: initialRecipes});
-        }
-        console.log("Set state > Did mount");
-    }
-
-    componentDidUpate(prevState, prevProps){
-        if(prevState.recipes !== this.state.recipes){
-            localStorage.setItem('recipes', JSON.stringify(this.state.recipes));
-        }
-        console.log("set Item > updated")
-    }
-
-    addRecipe = newRecipe => {
-        this.setState(prevState => ({
-            recipes: [...prevState.recipes, newRecipe],
-        }))
-    }
-
-    deleteRecipe = recipeId => {
-        this.setState(prevState => ({
-            recipes: prevState.recipes.filter(recipe => recipe.id !== recipeId),
-        }));
-    };
+      };
 
     render() {
-        console.log("render method");
-        return (
+        const {dog, isLoading, error} = this.state;
+        return(
             <Layout>
-                <RecipeForm onSave={this.addRecipe}></RecipeForm>
-                <RecipeList items={this.state.recipes} onDelete={this.deleteRecipe}></RecipeList>
-                <GlobalStyle />
+                <BreedSelect onSelect={this.fetchDog} />
+                 {isLoading && <DogSkeleton />}
+                 {error && <ErrorMessage />}
+                 {dog && !isLoading && <Dog dog={dog} />}
             </Layout>
         )
     }
 }
 
 
-///Lesson 5 handling render errors
-// export class App extends Component{
-//     state = {hasError: false};
 
-//     componentDidCatch(error, info){
-//         //If this method was called, then there is an error.
-//         //set the state
-//         this.setState({hasError: true});
-//         //you can also send an error report to yoiur analytics service
-//         //logErrorToMYService(error, info);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from "react";
+// import Api from '../services/Api';
+
+// const ArticleList = ({ articles }) => (
+//   <ul>
+//     {articles.map(({ objectID, url, title }) => (
+//       <li key={objectID}>
+//         <a href={url} target="_blank" rel="noreferrer noopener">
+//           {title}
+//         </a>
+//       </li>
+//     ))}
+//   </ul>
+// );
+
+// export class App extends Component {
+//   state = {
+//     articles: [],
+//     isLoading: false,
+//     error: null,
+//   };
+
+//   async componentDidMount() {
+
+//     try {
+//         this.setState({isLoading: true});
+//         const articles = Api.fetchArticlesWithQuery("react");
+//         this.setState({ articles });
+//       } catch (error) {
+//         this.setState({ error });
+//       } finally {
+//         this.setState({ isLoading: false });
+//       }
 //     }
 
-//     render() {
-//         if(this.state.hasError) {
-//             return <h1>Something went wrong, please try again later;</h1>
-//         }
-
-//         return this.props.children;
-//     }
+//   render() {
+//     const { articles, isLoading, error } = this.state;
+//     return (
+//       <div>
+//             {error && <p>Whoops, something went wrong: {error.message}</p>}
+//             {isLoading && <p>Loading...</p>}
+//             {articles.length > 0 && <ArticleList articles={articles}></ArticleList>}
+//       </div>
+//     );
+//   }
 // }
---
+++ src/components/BreedSelect.js
@@ -0,0 +1,47 @@
+import {Component} from "react";
+import Select from 'react-select';
+import { fetchBreeds} from "api";
+import { ErrorMessage } from  "./ErrorMessage";
+
+const ERROR_MSG = 
+    `Something went wrong, please reload page`;
+
+export class BreedSelect extends Component{
+    state = {
+        breeds: [],
+        isLoading: false,
+        error: null,
+    }
+
+    async componentDidMount() {
+        try{
+            this.setState({isLoading: true, erro: null});
+            const fetchedBreeds = await fetchBreeds();
+            this.setState({breeds: fetchedBreeds});
+        } catch (error) {
+            this.setState({error: ERROR_MSG})
+        }finally{
+            this.setState({isLoading: false})
+        }
+    }
+
+    render() {
+        const {breeds, isLoading, error} = this.state;
+        const options = breeds.map(breed => ({
+            value: breed.id,
+            label: breed.name,
+        }));
+
+        return (
+            <div>
+                <Select
+                    options={options}
+                    isLoading={isLoading}
+                    onChange={option => this.props.onSelect(option.value)}
+                >
+                    {error && <ErrorMessage>{error}</ErrorMessage>}
+                </Select>
+            </div>
+        )
+    }
+}
